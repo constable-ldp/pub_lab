6ac03081227cddf0755ca9833093b5257a4b1f29
Luke <constableldp@gmail.com>
2021-03-03 11:15:21 +0000

    updated till price

--- tests/pub_test.py
+++ tests/pub_test.py
@@ -4,13 +4,13 @@ from classes.pub import Pub
 
 class TestPub(unittest.TestCase):
     def setUp(self):
-        self.pub = Pub(1000, ['Tennets'])
+        self.pub = Pub(1000.00, ['Tennets'])
     
     def test_pub_has_name(self):
         self.assertEqual('The Royal Mile', self.pub.name)
     
     def test_pub_has_funds(self):
-        self.assertEqual(1000, self.pub.till)
+        self.assertEqual(1000.00, self.pub.till)
     
     def test_pub_has_drinks(self):
         self.assertEqual(['Tennets'], self.pub.drinks)
